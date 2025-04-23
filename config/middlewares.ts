38897111b7b6e16bcbe00e8f04f447c57fffa7a7
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-04-23 03:20:35 -0300

    FEATURE | Enviroment setted for production

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,20 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'https://rominafrola.com',        // tu frontend en producción
+        'https://www.rominafrola.com',    // si usas www
+        'http://localhost:3000',        // para desarrollo local
+      ],
+      headers: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
